b8ed520f8db70374e767568f2b854aff5189749e
hanbaopin <1021048439@qq.com>
2019-05-04 13:27:22 +0800

    第一次提交

---
+++ src/hadoop/MapTest.java
@@ -0,0 +1,24 @@
+package hadoop;
+
+import java.io.BufferedReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/*******************
+*@ClassName   Map.java
+*@Date        2019年4月29日-上午9:47:12
+*@Author      BP
+*@Version     V8
+*@Description
+*********************/
+public class MapTest {
+public static void main(String[] args) {
+	System.out.println("aaa");
+		
+	}
+
+
+
+
+}
+
